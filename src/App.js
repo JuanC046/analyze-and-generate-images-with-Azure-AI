04f5e3523fd85ba56dcf950e19acd2e55c825501
juanc <juan.lopez15@utp.edu.co>
2023-11-15 15:15:11 -0500

    Add style to my page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 function App() {
   const title = 'Computer Vision';
-  return <h1>{title}</h1>
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+      <input type='file' id='imageInput' accept='image/*' onChange={(e) => console.log(
+        e.target.files[0]
+      )} />
+      <button onClick={() => console.log('click')}>Analize</button>
+      <button onClick={() => console.log('click')}>Create</button>
+    </div>
+    );
+
+
 }
 
 export default App;

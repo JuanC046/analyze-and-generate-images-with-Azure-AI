bee23e0d8e7a27d5baa4c9480cf074da115ff87e
juanc <juan.lopez15@utp.edu.co>
2023-11-15 15:01:13 -0500

    Add title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const title = 'Computer Vision';
+  return <h1>{title}</h1>
 }
 
 export default App;
